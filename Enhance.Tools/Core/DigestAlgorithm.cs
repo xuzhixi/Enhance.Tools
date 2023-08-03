9e05b725a91a3333d8424d221fb1e74bb6845cb8
xuzhixi <zhixi.xu@qq.com>
2023-08-03 15:31:52 +0800

    1. 新增 - MD5工具类

---
+++ Enhance.Tools/Core/DigestAlgorithm.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Enhance.Tools
+{
+    public class DigestAlgorithm
+    {
+        /// <summary>
+        /// 计算MD5值（大写字母）
+        /// </summary>
+        /// <param name="data"></param>
+        /// <returns></returns>
+        public static string MD5(string data)
+        {
+            var md5 = System.Security.Cryptography.MD5.Create();
+            var hashBytes = md5.ComputeHash(System.Text.Encoding.UTF8.GetBytes(data));
+            return BitConverter.ToString(hashBytes).Replace("-", "");
+        }
+    }
+}
---
+++ Enhance.Tools/Core/StringExtension.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Enhance.Tools
+{
+    public static class StringExtension
+    {
+        public static string MD5Lower(this string data)
+        {
+            return DigestAlgorithm.MD5(data).ToLower();
+        }
+
+        public static string MD5Upper(this string data)
+        {
+            return DigestAlgorithm.MD5(data);
+        }
+    }
+}
